47f66f2520412454064e95e9de4925729b50afab
daniel diaz <akainooni@gmail.com>
2024-07-19 13:15:11 -0400

    chore: Refactor Solitaire card component

--- components/games/solitarie/card.vue
+++ components/games/solitarie/card.vue
@@ -1,9 +1,6 @@
-<template>
-    <button class="card" draggable="true">
-
+<template>   
         <img v-if="props.card.isFaceUp" :src="`/images/cards/${props.card.number}${props.card.type}.svg`" />
         <img v-else src="/images/cards/black-back.svg" />
-    </button>
 </template>
 <script setup lang="ts">
 import type { Card } from "~/types/game/solitarie";
@@ -13,26 +10,8 @@ const props = defineProps<{
 >();
 </script>
 <style scoped>
-.card {
-    max-width: 100%;
-    aspect-ratio: 5/7;
-    cursor: pointer;
-    position: absolute;
-    border:0;
-    padding: 0;
-    background-color: transparent;
-
-    img {
+img {
         max-width: 100%;
         max-height: 100%;
     }
-}
-
-.card:hover {
-    transform: translateY(-10px);
-}
-
-.card:focus {
-    border: 2px solid blue;
-}
 </style>
